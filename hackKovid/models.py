23384248c7795ee8b87712c77b21de0f2a6f5f8b
Coderaman07 <alexmercerr07@gmail.com>
2020-09-25 22:01:11 +0530

    Added Model for Adding the New Products

--- hackKovid/models.py
+++ hackKovid/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    ID = models.AutoField
+    Name = models.CharField(max_length = 50)
+    Price = models.CharField(max_length=20)
+    desc = models.CharField(max_length=5000, default='')
+    image = models.ImageField(upload_to='hackKovid/images')
+    UploadToHINTLatestProductOrTOPSellingProducts = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.Name
