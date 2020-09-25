13d93f32f3aba28a738b365dba25b8a1d6e91ccc
Coderaman07 <alexmercerr07@gmail.com>
2020-09-25 22:05:52 +0530

    Added new Url paths for this Project

--- hackKovid/urls.py
+++ hackKovid/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='Home'),
     path('store/', views.store, name='store'),
     path('guidelines/', views.guide, name='guide'),
-]
+    path('cart/', views.cart, name='cart'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('thankyou/', views.thankyou, name='thankyou'),
+    path('product/<stri>', views.products, name='product'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
