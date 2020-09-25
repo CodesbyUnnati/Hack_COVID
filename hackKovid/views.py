891dc7514c98e7cc10aaaa17b821fa0a3aef255c
Coderaman07 <alexmercerr07@gmail.com>
2020-09-25 22:06:25 +0530

    Coded the way of viewing the Products for diffrent Pages

--- hackKovid/views.py
+++ hackKovid/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse
+from .models import Product
 
 # Create your views here.
 
@@ -6,7 +7,24 @@ def home(request):
     return render(request, 'index.html')
 
 def store(request):
-    return render(request, 'store.html')
+    item = Product.objects.values()
+    allProds = {'item': item}
+    return render(request, 'store.html', allProds)
 
 def guide(request):
     return render(request, 'guidelines.html')
+
+def cart(request):
+    return render(request, 'cart.html')
+
+def thankyou(request):
+    return render(request, 'thankyou.html')
+
+def checkout(request):
+    return render(request, 'checkout.html')
+
+def products(request, stri):
+    allprods = Product.objects.values()
+    catprods = Product.objects.filter(id=stri)
+    allprod = {'allprod': allprods, 'catprod': catprods}
+    return render(request, 'shop-single.html', allprod)
