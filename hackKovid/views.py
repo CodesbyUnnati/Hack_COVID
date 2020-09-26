69219422525f859e8c2cce3e92abedca95607d2c
Coderaman07 <alexmercerr07@gmail.com>
2020-09-26 19:30:02 +0530

    Added a POST requests Handeler so that anyone can see that his Order has been Placed

--- hackKovid/views.py
+++ hackKovid/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponse
-from .models import Product
+from .models import Product, Order
 
 # Create your views here.
 
@@ -17,8 +17,22 @@ def guide(request):
 def cart(request):
     return render(request, 'cart.html')
 
-def thankyou(request):
-    return render(request, 'thankyou.html')
+def thankyou(request, methods = ["GET", "POST"]):
+    if request.method == 'POST':
+        country = request.POST.get('countryinput')
+        itemsJson = request.POST.get('itemsJson')
+        fname = request.POST.get('fname')
+        lname = request.POST.get('lname')
+        address = request.POST.get('address')
+        state = request.POST.get('state')
+        zipcode = request.POST.get('zip')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        amount = request.POST.get('amount')
+        order = Order(itemsJson=itemsJson, FirstName=fname, LastName=lname, email=email, address=address,
+                       state=state, postNumber=zipcode, phone=phone, amount=amount, country=country)
+        order.save()
+        return render(request, 'thankyou.html')
 
 def checkout(request):
     return render(request, 'checkout.html')
@@ -28,3 +42,13 @@ def products(request, stri):
     catprods = Product.objects.filter(id=stri)
     allprod = {'allprod': allprods, 'catprod': catprods}
     return render(request, 'shop-single.html', allprod)
+
+    # itemsJson = models.AutoField
+    # country = models.CharField(max_length=50)
+    # FirstName = models.CharField(max_length=30)
+    # LastName = models.CharField(max_length=30)
+    # address = models.CharField(max_length=50)
+    # state = models.CharField(max_length=20)
+    # postNumber = models.IntegerField()
+    # email = models.CharField(max_length=20)
+    # phone = models.CharField(max_length=20)
