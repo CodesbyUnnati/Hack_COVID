cc4a559e1606310a9e4c024b396d73d6236a1a6c
Coderaman07 <alexmercerr07@gmail.com>
2020-09-26 19:28:40 +0530

    registered the Order Model

--- hackKovid/admin.py
+++ hackKovid/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Order
 
 # Register your models here.
 admin.site.register(Product)
+admin.site.register(Order)
 
 admin.site.site_header = 'Hacking COVID'
